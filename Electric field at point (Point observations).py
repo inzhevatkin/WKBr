952deaff21af78ac67c3618155c53d239285e71e
inzhevatkin <k.inzhevatkin@yandex.ru>
2021-04-12 23:57:44 +0700

    Added new file (Compare2.py). Added implementation (Electric field on the z-axis.py). Minor changes (Electric field at point.py)

--- Electric field at point (Point observations).py
+++ Electric field at point (Point observations).py
@@ -1,63 +1,10 @@
-from Compare2 import scattnlay_bhfield_to_adda, return_coordinates
+from Compare2 import scattnlay_bhfield_to_adda, return_coordinates, point_field
 from WKBr import find_wkb_ef
 from math import isclose
 import os
 import shutil
 
 # This code prepares the electric field at a given point and outputs it to a file depending on the p parameter.
-
-
-# Function for finding the mean electric field at a point.
-# I usually use a small averaging radius and only one point falls into it. Search in a file path1, write to file f2.
-def point_field(path1, f2, xp, yp, zp, my_abs_tol, m):
-    f = open(path1)
-    f.readline()
-    e_aver, exr_av, exi_av, eyr_av, eyi_av, ezr_av, ezi_av = 0, 0, 0, 0, 0, 0, 0
-    count = 0
-    while True:
-        numbers = f.readline()
-        if len(numbers) == 0:  # Zero length denotes end of file (EOF)
-            break
-        numbers = numbers.split()
-        if len(numbers) <= 1:  # One more end-of-file check
-            break
-        x = float(numbers[0])
-        y = float(numbers[1])
-        z = float(numbers[2])
-        e = float(numbers[3]) ** 0.5
-        exr = float(numbers[4])
-        exi = float(numbers[5])
-        eyr = float(numbers[6])
-        eyi = float(numbers[7])
-        ezr = float(numbers[8])
-        ezi = float(numbers[9])
-        if isclose(x, xp, abs_tol=my_abs_tol) and \
-                isclose(y, yp, abs_tol=my_abs_tol) and \
-                isclose(z, zp, abs_tol=my_abs_tol):
-            e_aver += (exr ** 2 + exi ** 2 + eyr ** 2 + eyi ** 2 + ezr ** 2 + ezi ** 2) ** 0.5
-            exr_av += exr
-            exi_av += exi
-            eyr_av += eyr
-            eyi_av += eyi
-            ezr_av += ezr
-            ezi_av += ezi
-            count += 1
-    if count != 0:
-        e_aver /= count
-        exr_av /= count
-        exi_av /= count
-        eyr_av /= count
-        eyi_av /= count
-        ezr_av /= count
-        ezi_av /= count
-    f2.write(str(xp) + ' ' + str(yp) + ' ' + str(zp) + ' '
-             + str(m) + ' ' + str(e_aver) + ' '
-             + str(exr_av) + ' ' + str(exi_av) + ' '
-             + str(eyr_av) + ' ' + str(eyi_av) + ' '
-             + str(ezr_av) + ' ' + str(ezi_av) + ' '
-             + str(count) + '\n')
-    f.close()
-
 
 if __name__ == "__main__":
     my_abs_tol = 0.01
@@ -109,9 +56,9 @@ if __name__ == "__main__":
                 flag = False
                 f2 = open(path3, 'w')
                 f2.write('x y z m |E_av| exr_av exi_av eyr_av eyi_av ezr_av ezi_av dots_number \n')
-                point_ElField(path2, f2, xp, yp, zp, my_abs_tol, m)
+                point_field(path2, f2, xp, yp, zp, my_abs_tol, m)
             else:
-                point_ElField(path2, f2, xp, yp, zp, my_abs_tol, m)
+                point_field(path2, f2, xp, yp, zp, my_abs_tol, m)
         f2.close()
 
 
--- Electric field on th z-axis (Point observations).py
+++ Electric field on th z-axis (Point observations).py
@@ -1,17 +1,42 @@
-
+from Compare2 import point_field, return_coordinates, scattnlay_bhfield_to_adda
 
 if __name__ == "__main__":
     my_abs_tol = 0.01
     size = 200
-    radius=size/2
+    radius = size/2
     grid = 320
     xp = 0
     yp = 0.31348
     m = 1.145
+    path_main = "C:/Users/konstantin/Documents/main-script/"
+    path_to_dir = path_main + "data size " + str(size) + ", grid " + str(grid) + ", section/"
+    path_write = path_to_dir
 
-    for type in ["wkbr"]: #, "wkb", "wkbr"
+    for type in ["wkbr", "wkb", "bhfield"]:  # "wkbr"
         print("Current type: ", type)
-        path_main = "C:/Users/konstantin/Documents/main-script/"
-        path_to_dir = path_main + "data size " + str(size) + ", grid " + str(grid) + ", section/"
-        path_to_file = path_to_dir + type + "-" + str(size) + "-" + str(m) + "-" + str(grid) + ".dat"
-
+        if type == "bhfield":
+            path_to_file = path_to_dir + type + "-" + str(size) + "-" + str(m) + "-" + str(grid) + ".dat"
+            path_to_file2 = path_to_dir + type + "-adda-" + str(size) + "-" + str(m) + "-" + str(grid) + ".dat"
+            scattnlay_bhfield_to_adda(path_to_file, path_to_file2, type, size / 2, grid)
+            path_to_file = path_to_file2
+        else:
+            path_to_file = path_to_dir + type + "-" + str(size) + "-" + str(m) + "-" + str(grid) + ".dat"
+        x, y, z = return_coordinates(path_to_file, type)
+        flag = True
+        prev = 0
+        for zp in z:
+            cur = zp
+            if cur == prev:
+                continue
+            print("Current z: ", zp)
+            if flag:
+                flag = False
+                path_write += type + '-field-x' + str(xp) + '-y' + str(yp) + '.dat'
+                f2 = open(path_write, 'w')
+                f2.write('x y z m |E_av| exr_av exi_av eyr_av eyi_av ezr_av ezi_av dots_number \n')
+                point_field(path_to_file, f2, xp, yp, zp, my_abs_tol, m)
+            elif flag is False:
+                point_field(path_to_file, f2, xp, yp, zp, my_abs_tol, m)
+            else:
+                print("Error! flag is not defined.")
+            prev = cur
