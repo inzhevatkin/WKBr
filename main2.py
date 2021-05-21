3c80e5cb9c0915574f60c4366d9e3f7fcbd270ab
inzhevatkin <k.inzhevatkin@yandex.ru>
2021-05-21 14:09:48 +0700

    Added new file for calculation of the WKBr electric field on the cluster.

---
+++ main2.py
@@ -0,0 +1,19 @@
+#!/opt/shared/anaconda/anaconda3/bin/python
+from WKBr import find_wkb_ef
+
+# The program prepares the WKBr field.
+size = 100
+grid = 160
+mi = 0
+mlist = [1.01, 1.05, 1.1]
+mytype = "complex_wkbr"
+path = "/mnt/scratch/ws/kginzhevatkin/202107181324ws/"
+
+if __name__ == "__main__":
+    for m in mlist:
+        print("Current size = ", size)
+        print("Current grid = ", grid)
+        print("Current m = ", m)
+        tail = str(size) + "-" + str(m) + "-" + str(mi) + "-" + str(grid) + ".dat"
+        find_wkb_ef(0, 0, 0, m, mi, size / 2, 1, path + mytype + "-" + tail, grid, type=mytype,
+                    find_grid=True)
