7a127693479b6fc1f084a119f72e180613c253c5
inzhevatkin <k.inzhevatkin@yandex.ru>
2021-05-21 17:02:02 +0700

    Added new functions for cluster calculations

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 #!/opt/shared/anaconda/anaconda3/bin/python
 from WKBr import find_wkb_ef
+from rearrange_ef_file import rearrange_ef
 
 # The program prepares the WKBr field.
 size = 100
@@ -15,5 +16,9 @@ if __name__ == "__main__":
         print("Current grid = ", grid)
         print("Current m = ", m)
         tail = str(size) + "-" + str(m) + "-" + str(mi) + "-" + str(grid) + ".dat"
-        find_wkb_ef(0, 0, 0, m, mi, size / 2, 1, path + mytype + "-" + tail, grid, type=mytype,
-                    find_grid=True)
+        find_wkb_ef(0, 0, 0, m, mi, size / 2, 1, path + mytype + "-" + tail, grid, type=mytype, find_grid=True)
+
+    # change columns to get y-polarization:
+    for m in mlist:
+        tail = str(size) + "-" + str(m) + "-" + str(mi) + "-" + str(grid)
+        rearrange_ef(path + mytype + "-" + tail + ".dat", path + mytype + "-" + tail + "-Y-component.dat")
